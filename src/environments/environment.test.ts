fc3b2b3aee1d0ab9e26d12f0908a2625ad8e6f79
Helix GPO <helix.gpo@gmail.com>
2025-09-12 12:41:57 +0200

    environments + config

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -3,5 +3,5 @@ export const environment = {
   ENVIRONMENT: 'staging',
   PROJECT_SERVICE_BASE_URL: 'http://localhost:8084/api/v1/projects',
   TESTIMONIAL_SERVICE_BASE_URL: 'http://localhost:8082/api/v1/testimonials',
-  API_KEY: '',
+  API_KEY: 'api_073e7bce-c02f-42b5-a64d-86c10342247d',
 };
